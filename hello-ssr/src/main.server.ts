685c929020ed4720796d446c2cd93766677c210c
JiaWei Lee <gsdukbh@gmail.com>
2021-05-26 09:40:19 +0800

    up

--- hello-ssr/src/main.server.ts
+++ hello-ssr/src/main.server.ts
@@ -1,26 +1,17 @@
 import type { RouteLocationRaw } from 'vue-router';
 import { buildApp } from './main.app';
 
-export default (url: RouteLocationRaw) =>
-  new Promise((resolve, reject) => {
+export async function render(url: RouteLocationRaw) {
+  return new Promise((resolve, reject) => {
     const { router, app, store } = buildApp();
     // 设置服务器端路由器的位置
-    router
-      .push(url)
-      .then(() => {
-        console.log('成功');
-      })
-      .catch((e) => {
-        console.log('错误 ', e);
-      });
+    router.push(url);
     router
       .isReady()
       .then(() => {
         const matchedComponents = router.currentRoute.value.matched;
-        console.log('ca', router.currentRoute.value.path);
         matchedComponents.flatMap((record) => {
           Object.values(record.components);
-          console.log(record.path);
         });
         // 没有匹配的路线，以404拒绝
         if (!matchedComponents.length) {
@@ -31,3 +22,4 @@ export default (url: RouteLocationRaw) =>
       })
       .catch(() => reject(new Error('503')));
   });
+}
--- vite-ssr-demo/vite.config.ts
+++ vite-ssr-demo/vite.config.ts
@@ -1,10 +1,9 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import';
-
-// import babel from 'vite-babel-plugin';
-
 import viteCompression from 'vite-plugin-compression';
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
     css: {
@@ -17,7 +16,6 @@ export default defineConfig({
     plugins: [
         vue(),
         viteCompression(),
-        // babel(),
         styleImport({
             libs: [
                 {
